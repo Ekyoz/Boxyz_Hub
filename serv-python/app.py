907450939529e438907fe4754e7511c2aa492a2e
Alexandre <alexandre.tresallet@gmail.com>
2021-04-20 19:05:58 +0200

    add serv python

--- serv-python/app.py
+++ serv-python/app.py
@@ -1,14 +1,37 @@
 from flask import Flask, request, render_template, session, redirect
 import requests
+import json
 
 app = Flask(__name__)
 
+remotejson = open("/home/alexandre/Bureau/Projet/Domotic/serv-python/remote.json", "r")
+remote=json.load(remotejson)
+url="http://192.168.1.29:3000/assistant"
+obj= {
+    "user": "Alexandre",
+    "command": "allume lampadaire alex"
+}
+
 #--------------------------------------------Home----------------------------------------#
-@app.route('/', methods = ['GET'])
+@app.route('/remote', methods = ['GET'])
 def home():
-#----------Code-------------------#
+    remoteid = request.args.get('id')
+    if remoteid is None:
+        return remote["remote"]
+    if remoteid is not None:
+        return remote["remote"][remoteid]
 
-
+@app.route('/status', methods = ['GET','POST'])
+def status():
+    status = request.args.get('stat')
+    if status == "on":
+        requests.post(url=url, data=obj)
+        print("work")
+        return "ok"
+    elif status == "off":
+        return "not ok"
+    else:
+        return "Argument missing"
 
 if __name__ == "__main__":
     app.run(port=3030, host='0.0.0.0', debug=True)
