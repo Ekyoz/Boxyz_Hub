362296207c8aec8145650200a4273b65be277753
Alexandre <alexandre.tresallet@gmail.com>
2021-04-23 14:41:25 +0200

    v1.2.22

--- serv-python/app.py
+++ serv-python/app.py
@@ -10,11 +10,8 @@ remote = json.load(open("/home/pi/Boxyz/serv-python/remote.json", "r"))
 for num in remote.keys():
     remote_num.append(num)
 
-remote["bc:dd:c2:55:63"] = "name"
+remote["bc:dd:c2:55:63"] = {"name" : "test", "func" : "test", "ip" : "192.168.1.25"}
 print(remote)
-
-print(remote_num)
-
 
 url="http://192.168.1.29:3000/assistant"
 
