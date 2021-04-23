332b51c347a3f086929fb25d59a551373864982b
Alexandre <alexandre.tresallet@gmail.com>
2021-04-23 17:45:14 +0200

    v1.2.30.32

--- serv-python/app.py
+++ serv-python/app.py
@@ -31,9 +31,9 @@ def remote():
     if info == "num":
         return str(num)
     if info == "json":
-        return str(open(access, "r"))
+        return int(open(access, "r"))
     elif info == None:
-        return str(open(access, "r")), str(num)
+        return int(open(access, "r")), str(num)
 
 
 @app.route('/button', methods = ['GET','POST'])
