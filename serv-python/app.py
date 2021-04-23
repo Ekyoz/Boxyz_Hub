d22b0aec75a8125df772448599f722b67fe50717
Alexandre <alexandre.tresallet@gmail.com>
2021-04-23 17:33:46 +0200

    v1.2.30.23

--- serv-python/app.py
+++ serv-python/app.py
@@ -32,7 +32,7 @@ def remote():
     if info == "num":
         return str(num)
     if info == "json":
-        return open(access, "r")
+        return render_template(access)
     elif info == None:
         return open(access, "r"), str(num)
 
