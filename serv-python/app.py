b6459124488d04c1bfb2bf1e91bbcdf0f16e0fb0
Alexandre <alexandre.tresallet@gmail.com>
2021-04-23 17:42:13 +0200

    v1.2.30.29

--- serv-python/app.py
+++ serv-python/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, session, redirect, jsonify
 import requests
 import json
-from remote import addremote, delremote, remotenum, access
+from remote import addremote, delremote, remotenum, access, remote_num
 
 app = Flask(__name__)
 url="http://192.168.1.29:3000/assistant"
@@ -27,7 +27,7 @@ def del_remote():
 @app.route('/remote', methods=['GET'])
 def remote():
     info = request.args.get('info')
-
+    remote_num = []
     num = remotenum()
     if info == "num":
         return str(num)
