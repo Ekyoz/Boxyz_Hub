53703f4be5acb2946008f7b18975a87e842fb8a0
Alexandre <alexandre.tresallet@gmail.com>
2021-04-23 14:11:02 +0200

    v1.2.13

--- serv-python/app.py
+++ serv-python/app.py
@@ -21,7 +21,7 @@ def remote():
     if remoteid is not None:
         return remote["remote"][remoteid]
     '''
-    return int(str(remote["remote"]))
+    return str(int(remote["remote"]))
 
 @app.route('/button', methods = ['GET','POST'])
 def button():
