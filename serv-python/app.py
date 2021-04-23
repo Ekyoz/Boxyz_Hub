7bf3a5517d3baa66409429812716bd3dcac53091
Alexandre <alexandre.tresallet@gmail.com>
2021-04-23 14:23:10 +0200

    v1.2.16

--- serv-python/app.py
+++ serv-python/app.py
@@ -6,7 +6,10 @@ import json
 app = Flask(__name__)
 
 remote = json.load(open("/home/pi/Boxyz/serv-python/remote.json", "r"))
-print(remote["bc:dd:c2:55:63:67"])
+for num in remote.keys():
+    print(num)
+    
+
 
 url="http://192.168.1.29:3000/assistant"
 
@@ -15,24 +18,10 @@ url="http://192.168.1.29:3000/assistant"
 @app.route('/button', methods = ['GET','POST'])
 def button():
     status = request.args.get('stat')
-    mac = request.args.get('mac')
-    if status == "on":
-        requests.post(url=url, data={
-            "user": "Alexandre",
-            "command": "allume lampadaire alex"
-        })
-        print("work")
-        print(mac)
-        return "ok"
-    elif status == "off":
-        requests.post(url=url, data={
-            "user": "Alexandre",
-            "command": "eteint lampadaire alex"
-        })
-        print("work")
-        return "ok"
-    else:
-        return "Argument missing"
+    ip = request.args.get('mac')
+    requests.post(url=url, data=remote)
+
+
 
 if __name__ == "__main__":
     app.run(port=3030, host='0.0.0.0', debug=True)
