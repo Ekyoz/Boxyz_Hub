fffc16849440d98083536e574cf590a67b0bbc7d
Alexandre <alexandre.tresallet@gmail.com>
2021-04-23 19:44:16 +0200

    v1.2.4.10

--- serv-python/remote.py
+++ serv-python/remote.py
@@ -1,7 +1,7 @@
 import json
 from os import access
 
-access = "../serv-python/remote.json"
+access = "serv-python/remote.json"
 remote_num =  []
 
 def remotenum():
@@ -16,16 +16,9 @@ def remotenum():
 def addremote(mac, name, func_on, func_off, ip):
     with open(access, "r") as f:
         remote = json.load(f)
-    try:
-        if mac and name and func_on and func_off and ip is not None:
-            with open(access, "w") as f:
-                remote[mac] = {"name" : str(name), "func_on" : str(func_on),"func_off" : str(func_off) , "ip" : str(ip)}
-                json.dump(remote, f, indent=6)
-        elif mac or name or func_on or func_off or ip is None:
-            return 'Error!'
-    except:
-        return 'Error!'
-
+    with open(access, "w") as f:
+        remote[mac] = {"name" : str(name), "func_on" : str(func_on),"func_off" : str(func_off) , "ip" : str(ip)}
+        json.dump(remote, f, indent=6)
 
 def delremote(keys):
     with open(access, "r") as f:
@@ -34,4 +27,5 @@ def delremote(keys):
     with open(access, "w") as f:
         remote.pop(keys, None)
         json.dump(remote, f, indent=6)
+
 
