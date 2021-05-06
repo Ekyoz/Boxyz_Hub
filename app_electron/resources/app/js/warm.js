9a4f425d7290ca19626f480524579d288a6fb97e
Ekyoz <75211316+Ekyoz@users.noreply.github.com>
2021-05-06 20:34:42 +0200

    Delete app_electron directory

--- app_electron/resources/app/js/warm.js
+++
@@ -1,9 +0,0 @@
-var sensor = require("node-dht-sensor");
-
-function temp(){
-    sensor.read(11, 4, function(err, temperature, humidity) {
-        if (!err) {
-            console.log(`temp: ${temperature}°C, humidity: ${humidity}%`);
-        }
-    });
-}
